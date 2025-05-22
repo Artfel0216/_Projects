44e2c99d8e11ab1691579f920a8bc0365fd3121f
Arthur Fellipe <arthur.fellipe.brandao@gmail.com>
2025-05-22 16:03:36 -0300

    generate-json

---
+++ genarate-json/app/page.tsx
@@ -0,0 +1,9 @@
+import Converter from '@/components/Converter';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
+      <Converter />
+    </main>
+  );
+}
