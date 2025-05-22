fd529c35af58a6b66b139e9e27bdf5cc9a0091e4
Arthur Fellipe <arthur.fellipe.brandao@gmail.com>
2025-05-22 16:06:37 -0300

    product-store

---
+++ product-store/lib/data.ts
@@ -0,0 +1,10 @@
+export const products = [
+  { id: '1', name: 'Tênis Esportivo', price: 199.90, description: 'Tênis leve e confortável', image: '/tenis.jpg' },
+  { id: '2', name: 'Camisa Polo', price: 99.90, description: 'Camisa polo elegante', image: '/camisa.jpg' },
+  // ...
+];
+export const categories = [
+  { id: '1', name: 'Roupas', image: '/roupas.jpg' },
+  { id: '2', name: 'Calçados', image: '/calcados.jpg' },
+  // ...
+];
