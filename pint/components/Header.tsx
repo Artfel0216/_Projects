9c36e987c80166b8554e102ae9e81febcd33f918
Arthur Fellipe <arthur.fellipe.brandao@gmail.com>
2025-05-22 16:06:18 -0300

    pint

---
+++ pint/components/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => (
+  <header className="text-center p-4 bg-gray-800 text-white">
+    <h1 className="text-2xl font-bold">Desenho Rápido</h1>
+  </header>
+);
+
+export default Header;
