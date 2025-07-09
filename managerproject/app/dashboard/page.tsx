12cc83637e8c6d9e939e50782396104f7c4c0402
Arthur Fellipe <arthur.fellipe.brandao@gmail.com>
2025-07-09 12:28:28 -0300

    update

--- managerproject/app/dashboard/page.tsx
+++ managerproject/app/dashboard/page.tsx
@@ -4,6 +4,12 @@ export default function dashboard() {
     return(
         <div>
             <Header />
+
+            <h1 className="font-bold text-[2rem] mt-[2rem] ml-[3rem]">Welcome, Arthur Fellipe 👨🏽‍💻</h1>
+
+            <div className="w-[30rem] h-[20rem] bg-black mt-[2rem] ml-[3rem] font-bold text-[1.3rem] rounded text-white p-[2rem] shadow-2xl">
+                <h1>📌 Recent:</h1>
+            </div>
         </div>
     )
 }
