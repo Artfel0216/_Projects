2b390de71197949c94b75cc578b0be0088b2cbc8
Arthur Fellipe <153021829+Artfel0216@users.noreply.github.com>
2026-04-04 15:35:44 -0300

    Delete weather directory

--- weather/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import WeatherContainer from '@/components/Weather/WeatherContainer';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <WeatherContainer />
-    </main>
-  );
-}
