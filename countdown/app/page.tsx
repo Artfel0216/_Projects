952597c6c324318bf087e973826927df6aff3b55
Arthur Fellipe <153021829+Artfel0216@users.noreply.github.com>
2026-04-04 15:34:53 -0300

    Delete countdown directory

--- countdown/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Countdown from '@/components/Countdown';
-
-export default function Home() {
-  return (
-    <main className="bg-[#1e1e2f] min-h-screen flex items-center justify-center">
-      <Countdown />
-    </main>
-  );
-}
