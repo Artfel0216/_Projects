0c3a685ae8a916a1189a94ff5749e024108bc440
Arthur Fellipe <153021829+Artfel0216@users.noreply.github.com>
2026-04-04 15:35:21 -0300

    Delete genarate-password directory

--- genarate-password/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import GeneratorContainer from '@/components/PasswordGenerator/GeneratorContainer';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <GeneratorContainer />
-    </main>
-  );
-}
