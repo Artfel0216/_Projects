1b608cb0f7e5fc8a524a003eb43a2b7a78d35cbb
Arthur Fellipe <153021829+Artfel0216@users.noreply.github.com>
2026-04-04 15:36:18 -0300

    Delete pint directory

--- pint/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-const Header = () => (
-  <header className="text-center p-4 bg-gray-800 text-white">
-    <h1 className="text-2xl font-bold">Desenho Rápido</h1>
-  </header>
-);
-
-export default Header;
