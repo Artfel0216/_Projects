9ccce27fe54ce118dd54010b837609907e945faa
Arthur Fellipe <153021829+Artfel0216@users.noreply.github.com>
2026-04-04 15:23:19 -0300

    Delete app-bd directory

--- app-bd/app/core/model/user.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-    id: string | number;
-    name: string,
-    email: string,
-    password: string,
-}
--- app-bd/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Page from "./components/templates/page";
-
-export default function Home() {
-  return (
-    <div>
-      <Page>opa</Page>
-    </div>
-  );
-}
