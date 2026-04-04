0df9eafd61ef78d03bd8d61e2ececf373ad4b458
Arthur Fellipe <153021829+Artfel0216@users.noreply.github.com>
2026-04-04 15:35:02 -0300

    Delete cristmas directory

--- cristmas/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import LightsController from '@/components/LightsController';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <LightsController />
-    </main>
-  );
-}
