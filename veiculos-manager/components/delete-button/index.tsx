cafd520b7b3476c54ac35e74c91fa1c77b8137aa
Arthur Fellipe <153021829+Artfel0216@users.noreply.github.com>
2026-04-04 15:36:34 -0300

    Delete veiculos-manager directory

--- veiculos-manager/components/delete-button/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function DeleteButton() {
-    return(
-        <button 
-    className="bg-[#000] w-[6rem] h-[3rem] rounded flex items-center justify-center font-bold text-[#fff] cursor-pointer hover:animate-jump animate-once animate-ease-linear">
-            Excluir
-        </button>
-    )
-}
