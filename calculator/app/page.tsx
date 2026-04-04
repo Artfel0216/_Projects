42bb36dfd4bd9e478b0c767ce583fac481b8572b
Arthur Fellipe <153021829+Artfel0216@users.noreply.github.com>
2026-04-04 15:30:32 -0300

    Delete calculator directory

--- calculator/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Calculator from '@/components/Calculatro';
-
-export default function Home() {
-  return (
-    <main className="bg-[#1e1e2f] flex items-center justify-center min-h-screen m-0 font-sans">
-      <Calculator />
-    </main>
-  );
-}
